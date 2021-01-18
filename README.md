46c3d0800a1ba5f0d8647801ef3d4d1f23d3d625
Fantasy Tu <f.tu@me.com>
2021-01-18 23:10:04 +0800

    Init Commit

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
-# homebridge-http-lock-ultimate
+# homebridge-http-qinlin
 
 [![npm](https://img.shields.io/npm/v/homebridge-http-lock-ultimate.svg)](https://www.npmjs.com/package/homebridge-http-lock-ultimate) [![npm](https://img.shields.io/npm/dt/homebridge-http-lock-ultimate.svg)](https://www.npmjs.com/package/homebridge-http-lock-ultimate)
 
 ## Description
 
-This [homebridge](https://github.com/nfarina/homebridge) plugin exposes a web-based locking device to Apple's [HomeKit](http://www.apple.com/ios/home/). Using simple HTTP requests with post, body and header support, the plugin allows you to lock/unlock the device, i.e. Shelly 1 devices.
+This plugin [homebridge](https://github.com/nfarina/homebridge) uses the Qinlin Wechat Miniprogram API to open doors.
 
 ## Installation
 
 1. Install [homebridge](https://github.com/nfarina/homebridge#installation-details)
-2. Install this plugin: `npm install -g homebridge-http-lock-ultimate`
+2. Install this plugin: `npm install -g homebridge-http-lock-qinlin`
 3. Update your `config.json` file
 
 ## Configuration examples
@@ -18,69 +18,9 @@ Configuration example for a Shelly 1 device controlled via Get Requests in local
 ```json
 "accessories": [
      {
-      "accessory": "HTTPLockUltimate",
+      "accessory": "HTTPLockQinLin",
       "name": "Front Door",
-      "resetLock": "true",
-      "resetLockTime": "5",
-      "http_method": "GET",
-      "openURL": "http://192.168.X.XX/relay/0?turn=on",
-      "closeURL": "http://192.168.X.XX/relay/0?turn=off"
+      "sessionId": "2c9a195976f6edc5017715ba89c73fb3"
       }
 ]
 ```
-
-This is a configuration example for a Shelly 1 device controlled via Post Requests to Shelly Cloud. Get's locked automatically after 5 Seconds.
-
-```json
-"accessories": [
-     {
-      "accessory": "HTTPLockUltimate",
-      "name": "Front Door",
-      "autoLock": "true",
-      "autoLockDelay": "5",
-      "http_method": "POST",
-      "openURL": "https:/your.server.address/device/relay/control/",
-      "openHeader": {
-          "Content-Type": "application/x-www-form-urlencoded"
-      },
-      "openBody": "turn=on&channel=0&id=XXXXXXX&auth_key=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-      "closeURL": "https://your.server.address/device/relay/control/",
-      "closeHeader": {
-          "Content-Type": "application/x-www-form-urlencoded"
-      },
-      "closeBody": "turn=off&channel=0&id=XXXXXXX&auth_key=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-      }
-]
-```
-
-### Core
-| Key | Description | Default |
-| --- | --- | --- |
-| `accessory` | Must be `HTTPLockUltimate` | N/A |
-| `name` | Name to appear in the Home app | N/A |
-| `openURL` | URL to trigger unlock | N/A |
-| `closeURL` | URL to trigger lock | N/A |
-
-### Optional fields
-| Key | Description | Default |
-| --- | --- | --- |
-| `autoLock` _(optional)_ | Whether your lock should re-lock after being opened (closeURL Request gets triggered) | `false` |
-| `autoLockDelay` _(optional)_ | Time (in seconds) until your lock will auto lock if enabled | `5` |
-| `resetLock` _(optional)_ | If your lock is locking itself after opened, use this option to reset the state automatically (Will not call closeURL and is ignored when using autoLock) | `false` |
-| `resetLockTime` _(optional)_ | Time (in seconds) until your lock will be set to locked | `5` |
-
-### Additional options
-| Key | Description | Default |
-| --- | --- | --- |
-| `timeout` _(optional)_ | Time (in seconds) until the accessory will be marked as _Not Responding_ if it is unreachable | `5` |
-| `http_method` _(optional)_ | HTTP method used to communicate with the device | `GET` |
-| `openHeader` | Request Header to send in unlock request | N/A |
-| `openBody` | JSON Body to send on open | N/A |
-| `closeHeader` | Request Header to send in lock request | N/A |
-| `closeBody` | JSON Body to send on close | N/A |
-| `username` _(optional)_ | Username if HTTP authentication is enabled | N/A |
-| `password` _(optional)_ | Password if HTTP authentication is enabled | N/A |
-| `model` _(optional)_ | Appears under the _Model_ field for the accessory | plugin |
-| `serial` _(optional)_ | Appears under the _Serial_ field for the accessory | version |
-| `manufacturer` _(optional)_ | Appears under the _Manufacturer_ field for the accessory | author |
-| `firmware` _(optional)_ | Appears under the _Firmware_ field for the accessory | version |
--- index.js
+++ index.js
@@ -5,126 +5,120 @@ const packageJson = require('./package.json')
 module.exports = function (homebridge) {
   Service = homebridge.hap.Service
   Characteristic = homebridge.hap.Characteristic
-  homebridge.registerAccessory('homebridge-http-lock-ultimate', 'HTTPLockUltimate', HTTPLockUltimate)
+  homebridge.registerAccessory('homebridge-http-lock-qinlin', 'HTTPLockQinLin', HTTPLockQinLin)
 }
 
-function HTTPLockUltimate (log, config) {
-  this.log = log
+class HTTPLockQinLin {
+  constructor(log, config) {
+    this.log = log
 
-  this.name = config.name
+    this.name = config.name
 
-  this.manufacturer = config.manufacturer || packageJson.author.name
-  this.serial = config.serial || packageJson.version
-  this.model = config.model || packageJson.name
-  this.firmware = config.firmware || packageJson.version
+    this.manufacturer = config.manufacturer || packageJson.author.name
+    this.serial = config.serial || packageJson.version
+    this.model = config.model || packageJson.name
+    this.firmware = config.firmware || packageJson.version
 
-  this.username = config.username || null
-  this.password = config.password || null
-  this.timeout = (config.timeout * 1000) || 5000
-  this.http_method = config.http_method || 'GET'
+    this.timeout = config.timeout || 5000
+    this.http_method = 'POST'
 
-  this.openURL = config.openURL
-  this.openBody = config.openBody || ''
-  this.openHeader = config.openHeader || null
-  this.closeURL = config.closeURL
-  this.closeBody = config.closeBody || ''
-  this.closeHeader = config.closeHeader || null
+    this.sessionId = config.sessionId
 
-  this.autoLock = config.autoLock || false
-  this.autoLockDelay = config.autoLockDelay || 5
-  
-  this.resetLock = config.resetLock || false
-  this.resetLockTime = config.resetLockTime || 5
+    this.autoLock = config.autoLock || true
+    this.autoLockDelay = config.autoLockDelay || 10
 
-  if (this.username != null && this.password != null) {
-    this.auth = {
-      user: this.username,
-      pass: this.password
-    }
+    this.refreshSessionTimeBuffer = config.refreshSessionTimeBuffer || 300
+
+    this.service = new Service.LockMechanism(this.name)
   }
 
-  this.log(this.name)
+  static get refreshSessionURL() {
+    return 'https://mobileapi.qinlinkeji.com/api/wxmini/v3/appuser/refresh'
+  }
 
-  this.service = new Service.LockMechanism(this.name)
-}
+  static get openDoorURL() {
+    return 'https://mobileapi.qinlinkeji.com/api/open/doorcontrol/v2/open'
+  }
 
-HTTPLockUltimate.prototype = {
-
-  identify: function (callback) {
+  identify (callback) {
     this.log('Identify requested!')
     callback()
-  },
+  }
 
-  _httpRequest: function (url, headers, body, method, callback) {
+  _httpRequest (url, headers, body, method, callback) {
     request({
       url: url,
-      headers: headers,
-      body: body,
       method: this.http_method,
       timeout: this.timeout,
-      rejectUnauthorized: false,
-      auth: this.auth
+      rejectUnauthorized: false
     },
     function (error, response, body) {
       callback(error, response, body)
     })
-  },
+  }
 
-  setLockTargetState: function (value, callback) {
+  setLockTargetState (value, callback) {
     var url
     var body
     var headers
     this.log('[+] Setting LockTargetState to %s', value)
-    if (value === 1) {
-      url = this.closeURL
-      body = this.closeBody
-      headers = this.closeHeader
-    } else {
-      url = this.openURL
-      body = this.openBody
-      headers = this.openHeader
-    }
-    this._httpRequest(url, headers, body, this.http_method, function (error, response, responseBody) {
-      if (error) {
-        this.log('[!] Error setting LockTargetState: %s', error.message)
-        callback(error)
-      } else {
-        if (value === 1) {
-          this.service.setCharacteristic(Characteristic.LockCurrentState, Characteristic.LockCurrentState.SECURED)
-          this.log('[*] Closed the lock')
-        } else {
-          this.log('[*] Opened the lock')
-          this.service.setCharacteristic(Characteristic.LockCurrentState, Characteristic.LockCurrentState.UNSECURED)
-          if (this.autoLock) {
-            this.autoLockFunction()
-          }
-		  else if (this.resetLock) {
-			  this.resetLockFunction()
-		  }
-        }
-        callback()
-      }
-    }.bind(this))
-  },
 
-  autoLockFunction: function () {
+    if (value === 1) {
+      this.service.setCharacteristic(Characteristic.LockCurrentState, Characteristic.LockCurrentState.SECURED)
+      this.log('[*] Closed the lock')
+      return
+    } else {
+      var openDoorURLWithSession = HTTPLockQinLin.openDoorURL + '?sessionId=wxmini:' + this.sessionId
+
+      this._httpRequest(openDoorURLWithSession, null, '', this.http_method, function (error, response, responseBody) {
+          if (error) {
+            this.log('[!] Error setting LockTargetState: %s', error.message)
+            callback(error)
+          }
+          else {
+            this.service.setCharacteristic(Characteristic.LockCurrentState, Characteristic.LockCurrentState.UNSECURED)
+            this.log('[*] Opened the lock')
+          }
+      }.bind(this))
+
+      if (this.autoLock) {
+        this.autoLockFunction()
+      }
+
+      callback()
+    }
+  }
+
+  refreshSession () {
+    var refreshSessionURLWithSession = HTTPLockQinLin.refreshSessionURL + '?sessionId=wxmini:' + this.sessionId
+
+    this._httpRequest(refreshSessionURLWithSession, null, '', this.http_method, function (error, response, responseBody) {
+        if (error) {
+          this.log('[!] Error refreshing session: %s', error.message)
+        }
+        else {
+          var result = JSON.parse(responseBody)
+          if (result.code === 0) {
+            this.sessionId = result.data.sessionId
+            this.log('[*] Refreshed Session: %s', this.sessionId)
+
+            setTimeout(() => {
+              this.refreshSession()
+            }, (result.data.expires - this.refreshSessionTimeBuffer) * 1000)
+          }
+        }
+    }.bind(this))
+  }
+
+  autoLockFunction () {
     this.log('[+] Waiting %s seconds for autolock', this.autoLockDelay)
     setTimeout(() => {
       this.service.setCharacteristic(Characteristic.LockTargetState, Characteristic.LockTargetState.SECURED)
       this.log('[*] Autolocking')
     }, this.autoLockDelay * 1000)
-  },
-  
-  resetLockFunction: function () {
-    this.log('[+] Waiting %s seconds for resetting lock state to locked', this.resetLockTime)
-    setTimeout(() => {
-	  this.service.getCharacteristic(Characteristic.LockCurrentState).updateValue(1)
-	  this.service.getCharacteristic(Characteristic.LockTargetState).updateValue(1)
-      this.log('[*] Lock State resetted')
-    }, this.resetLockTime * 1000)
-  },
+  }
 
-  getServices: function () {
+  getServices () {
     this.service.setCharacteristic(Characteristic.LockCurrentState, Characteristic.LockCurrentState.SECURED)
     this.service.setCharacteristic(Characteristic.LockTargetState, Characteristic.LockTargetState.SECURED)
 
@@ -139,6 +133,9 @@ HTTPLockUltimate.prototype = {
       .getCharacteristic(Characteristic.LockTargetState)
       .on('set', this.setLockTargetState.bind(this))
 
+    this.refreshSession()
+
     return [this.informationService, this.service]
   }
+
 }
